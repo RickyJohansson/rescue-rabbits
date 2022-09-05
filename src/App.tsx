c6401602eb39e78b2b6840730e2764b326f47855
RickyJohansson <Rickjoh91@hotmail.com>
2022-09-05 14:45:48 +0200

    Fixed Nav and Location components

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
-<<<<<<< Updated upstream
 import Header from './components/Header';
 
-=======
 import Nav from "./components/Nav";
 import Location from "./components/Locations";
->>>>>>> Stashed changes
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-<<<<<<< Updated upstream
       <Header />
       <nav>
         <h1>NAVIGERING</h1>
@@ -25,12 +21,10 @@ function App() {
           <li>HAMSTRAR</li>
         </ul>
       </nav>
-=======
       <header>
         <h1>Rescue Rabbits</h1>
       </header>
       <Nav />
->>>>>>> Stashed changes
       <Main />
       <Location />
 
