acf02668e21bc829865108dcfab1e11c8ce0439e
Hampus83 <jhmacgyver@gmail.com>
2022-09-05 14:56:48 +0200

    småpill

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import Main from "./components/Main";
 import Nav from "./components/Nav";
 import Location from "./components/Locations";
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
+
     <div className="App">
       <Header />
       <Nav />
       <Main />
+      <Location />
       <footer>
         <h1>Rescue Rabbits</h1>
       </footer>
     </div>
+    
   );
 }
 
