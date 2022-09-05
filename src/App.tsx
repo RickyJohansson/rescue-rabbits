efe0183332ee70cf0a9978ab2507147451dd756b
Williamlof <83575469+Williamlof@users.noreply.github.com>
2022-09-05 13:19:55 +0200

    Holy-grail layout made
    Holy-grail layout made

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,126 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      
+      <header>
+        <h1>Rescue Rabbits</h1>
+      </header>
+
+      <nav>
+        <h1>NAVIGERING</h1>
+        <ul>
+          <li>HUNDAR</li>
+          <li>KATTER</li>
+          <li>MARKATTOR</li>
+          <li>MINIGRISAR</li>
+          <li>HAMSTRAR</li>
+        </ul>
+      </nav>
+
+      <main>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+        <article className="animalCard">
+          <h1>Charles</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut,
+            sapiente.
+          </p>
+        </article>
+      </main>
+
+      <aside>
+        <h1>HÄR FINNS VI</h1>
+        <ul>
+          <li>Stockholm</li>
+          <li>Göteborg</li>
+          <li>Karlstad</li>
+          <li>Linköping</li>
+        </ul>
+      </aside>
+
+      <footer>
+        <h1>Rescue Rabbits</h1>
+      </footer>
     </div>
   );
 }
--- src/components/Locations.tsx
+++ src/components/Locations.tsx
@@ -1,8 +1,5 @@
 function Locations() {
-    return (
-        <>
-        </>
-    );
+  return <></>;
 }
 
 export default Locations;
