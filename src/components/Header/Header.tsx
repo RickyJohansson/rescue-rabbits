46de524d235e4569308a3681b4b8f049877fff29
RickyJohansson <Rickjoh91@hotmail.com>
2022-09-12 16:20:21 +0200

    fixed search button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -39,7 +39,6 @@ function Header({ setAnimals }: Props) {
       });
       setAnimals(newFilteredAnimals);
     }
-    event.preventDefault();
   }
 
   const handleSearchButton: () => void = () => {
